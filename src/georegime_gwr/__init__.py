27c75fc9f8636f19f850412853b1fdba432c8318
Jinghao Hu <731409213@qq.com>
2026-08-30 23:54:14 +0800

    model: export unified regime-aware GWR baseline

--- src/georegime_gwr/__init__.py
+++ src/georegime_gwr/__init__.py
@@ -5,6 +5,14 @@ method development and paper experiments rather than a mature software API.
 """
 
 from .gwr import BasicGWR, GWRResult
+from .regime_gwr import RegimeAwareGWR, RegimeAwareGWRResult
 from .grgwr import GRGWRBaseline, GRGWRResult
 
-__all__ = ["BasicGWR", "GWRResult", "GRGWRBaseline", "GRGWRResult"]
+__all__ = [
+    "BasicGWR",
+    "GWRResult",
+    "RegimeAwareGWR",
+    "RegimeAwareGWRResult",
+    "GRGWRBaseline",
+    "GRGWRResult",
+]
